bc0fd9858e2e4b77d2900e1d2b911894f58355dc
karenioane-tech <karenmwaniki1@gmail.com>
2026-03-31 22:10:27 +0300

    "update index.html,sytle.css,main.js"

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,3 +55,45 @@ sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+
+const form = document.getElementById("contact-form");
+const status = document.getElementById("form-status");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  const data = new FormData(event.target);
+  
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      status.innerHTML = "Message sent successfully!";
+      status.style.color = "green";
+      form.reset();
+    } else {
+      response.json().then(data => {
+        status.innerHTML = "Oops! Something went wrong.";
+        status.style.color = "red";
+      });
+    }
+  }).catch(error => {
+    status.innerHTML = "Oops! Something went wrong.";
+    status.style.color = "red";
+  });
+}
+
+form.addEventListener("submit", handleSubmit);
+
+
+
+
+
+
+
+
+
+
